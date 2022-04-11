b5b973d70774792ad2285633817d39b20cd4d9f7
duart38 <duartsnel@icloud.com>
2022-04-11 14:37:47 +0200

    Feedback from leslie

--- contract_based_solution/donate.sol
+++ contract_based_solution/donate.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
+// TODO: import Ownable in fav of modifier
 
 contract KoiosDonation {
     address public owner;
